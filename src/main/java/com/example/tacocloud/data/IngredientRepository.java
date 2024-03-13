3793f70945c04c46fa11b5f14f0e56f474fd2dd4
Robert Oguzov <robertmama20@gmail.com>
2024-03-13 15:12:06 +0300

    Добавления переменной экземпляра jdbcTemplate в класс IngredientRepository

--- src/main/java/com/example/tacocloud/data/IngredientRepository.java
+++ src/main/java/com/example/tacocloud/data/IngredientRepository.java
@@ -1,11 +1,18 @@
 package com.example.tacocloud.data;
 
 import com.example.tacocloud.Ingredient;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 import java.util.Optional;
 
 public class IngredientRepository implements Repository<Ingredient> {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public IngredientRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public List<Ingredient> findAll() {
