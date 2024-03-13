b7462764fafb4d7de15cd4ec0d7fc76476e1193e
Robert Oguzov <robertmama20@gmail.com>
2024-03-13 15:08:35 +0300

    Создание реализации интерфейса Repository для Ingredient

---
+++ src/main/java/com/example/tacocloud/data/IngredientRepository.java
@@ -0,0 +1,25 @@
+package com.example.tacocloud.data;
+
+import com.example.tacocloud.Ingredient;
+
+import java.util.List;
+import java.util.Optional;
+
+public class IngredientRepository implements Repository<Ingredient> {
+
+
+    @Override
+    public List<Ingredient> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Ingredient> findById(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void save(Ingredient ingredient) {
+
+    }
+}
