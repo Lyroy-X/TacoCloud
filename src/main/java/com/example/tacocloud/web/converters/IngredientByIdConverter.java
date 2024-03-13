66e39efcc3a497def52774253e5da8a6260fbc93
Robert Oguzov <robertmama20@gmail.com>
2024-03-13 15:55:36 +0300

    Упрощения конвертора IngredientByIdConverter

--- src/main/java/com/example/tacocloud/web/converters/IngredientByIdConverter.java
+++ src/main/java/com/example/tacocloud/web/converters/IngredientByIdConverter.java
@@ -1,35 +1,24 @@
 package com.example.tacocloud.web.converters;
 
 import com.example.tacocloud.Ingredient;
-import com.example.tacocloud.Ingredient.Type;
+import com.example.tacocloud.data.Repository;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class IngredientByIdConverter implements Converter<String, Ingredient> {
 
-    private Map<Integer, Ingredient> ingredientMap = new HashMap<>();
+    private final Repository<Ingredient> ingredientRepository;
 
-    public IngredientByIdConverter() {
-        ingredientMap.put(0, new Ingredient(0, "Flour Tortilla", Type.WRAP));
-        ingredientMap.put(1, new Ingredient(1, "Corn Tortilla", Type.WRAP));
-        ingredientMap.put(2, new Ingredient(2, "Ground Beef", Type.PROTEIN));
-        ingredientMap.put(3, new Ingredient(3, "Carnitas", Type.PROTEIN));
-        ingredientMap.put(4, new Ingredient(4, "Diced Tomatoes", Type.VEGGIES));
-        ingredientMap.put(5, new Ingredient(5, "Lettuce", Type.VEGGIES));
-        ingredientMap.put(6, new Ingredient(6, "Cheddar", Type.CHEESE));
-        ingredientMap.put(7, new Ingredient(7, "Monterrey Jack", Type.CHEESE));
-        ingredientMap.put(8, new Ingredient(8, "Salsa", Type.SAUCE));
-        ingredientMap.put(9, new Ingredient(9, "Sour Cream", Type.SAUCE));
-        ingredientMap.put(10, new Ingredient(10, "Size", Type.SIZE));
+    @Autowired
+    public IngredientByIdConverter(Repository<Ingredient> ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
     }
 
     @Override
     public Ingredient convert(@NonNull String id) {
-        return ingredientMap.get(Integer.parseInt(id));
+        return ingredientRepository.findById(id).orElseThrow();
     }
 }
