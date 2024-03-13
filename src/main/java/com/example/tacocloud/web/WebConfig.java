b36f278011eb259e5c59ff3668088dc04f341cb6
Robert Oguzov <robertmama20@gmail.com>
2024-03-13 13:05:59 +0300

    Перенес создлание контролера для представления home в конфигурационный файл

--- src/main/java/com/example/tacocloud/web/WebConfig.java
+++ src/main/java/com/example/tacocloud/web/WebConfig.java
@@ -1,18 +1,16 @@
 package com.example.tacocloud.web;
 
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-@AutoConfiguration
 @ComponentScan("com.example.tacocloud")
 public class WebConfig implements WebMvcConfigurer {
 
-
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
 }
-
-
