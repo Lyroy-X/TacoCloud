72a57d8d5cf12e39c12b5e4d21eb975283491f70
Robert Oguzov <robertmama20@gmail.com>
2024-03-13 15:50:56 +0300

    Интегрирование ingredientRepository в класс TacoOrderController

--- src/main/java/com/example/tacocloud/web/TacoOrderController.java
+++ src/main/java/com/example/tacocloud/web/TacoOrderController.java
@@ -4,14 +4,15 @@ import com.example.tacocloud.Ingredient;
 import com.example.tacocloud.Ingredient.Type;
 import com.example.tacocloud.Taco;
 import com.example.tacocloud.TacoOrder;
+import com.example.tacocloud.data.Repository;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +21,18 @@ import java.util.stream.Collectors;
 @RequestMapping("/tof")
 @SessionAttributes("tacoOrder")
 public class TacoOrderController {
+
+    private final Repository<Ingredient> ingredientRepository;
+
+    @Autowired
+    public TacoOrderController(Repository<Ingredient> ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @GetMapping
+    public String showTacoOrderForm() {
+        return "tacoOrderForm";
+    }
 
     @PostMapping
     public String redirectCurrentOrder(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
@@ -31,30 +44,12 @@ public class TacoOrderController {
         return "redirect:orders/current";
     }
 
-    @GetMapping
-    public String showTacoOrderForm() {
-        return "tacoOrderForm";
-    }
-
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient(0, "Flour Tortilla", Type.WRAP),
-                new Ingredient(1, "Corn Tortilla", Type.WRAP),
-                new Ingredient(2, "Ground Beef", Type.PROTEIN),
-                new Ingredient(3, "Carnitas", Type.PROTEIN),
-                new Ingredient(4, "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient(5, "Lettuce", Type.VEGGIES),
-                new Ingredient(6, "Cheddar", Type.CHEESE),
-                new Ingredient(7, "Monterrey Jack", Type.CHEESE),
-                new Ingredient(8, "Salsa", Type.SAUCE),
-                new Ingredient(9, "Sour Cream", Type.SAUCE),
-                new Ingredient(10, "Small", Type.SIZE),
-                new Ingredient(10, "Medium ", Type.SIZE),
-                new Ingredient(10, "Big", Type.SIZE)
-        );
 
+        List<Ingredient> ingredients = ingredientRepository.findAll();
         Type[] types = Ingredient.Type.values();
+
         for (Type type : types) {
             model.addAttribute(type.toString().toLowerCase(),
                     filterByType(ingredients, type));
