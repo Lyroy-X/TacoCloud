4d4990dbeff4afe27bdcfea34a9df58d8b10682c
Robert Oguzov <robertmama20@gmail.com>
2024-03-16 17:33:48 +0300

    Рефактор конфигурации приложения

--- src/main/java/com/example/tacocloud/web/WebConfig.java
+++ src/main/java/com/example/tacocloud/web/WebConfig.java
@@ -1,12 +1,12 @@
-package com.example.tacocloud.web;
+package com.example.tacocloud;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ComponentScan("com.example.tacocloud")
+@EnableAutoConfiguration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
