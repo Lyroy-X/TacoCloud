0be68d68106020c9e2741ad0322fd1213c777d38
Robert Oguzov <robertmama20@gmail.com>
2024-03-23 16:08:50 +0300

    Редактирование IngredientRepository и OrderRepository

--- src/main/java/com/example/tacocloud/data/IngredientRepository.java
+++ src/main/java/com/example/tacocloud/data/IngredientRepository.java
@@ -1,7 +1,7 @@
 package com.example.tacocloud.data;
 
-import com.example.tacocloud.Ingredient;
+import com.example.tacocloud.entity.Ingredient;
 import org.springframework.data.repository.CrudRepository;
 
-public interface IngredientRepository extends CrudRepository<Ingredient, Integer> {
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 }
--- src/main/java/com/example/tacocloud/data/OrderRepository.java
+++ src/main/java/com/example/tacocloud/data/OrderRepository.java
@@ -1,6 +1,6 @@
 package com.example.tacocloud.data;
 
-import com.example.tacocloud.TacoOrder;
+import com.example.tacocloud.entity.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Integer> {
