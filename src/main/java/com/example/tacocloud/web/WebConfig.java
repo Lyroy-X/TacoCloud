42c3047fe25fe3cb40d46fcdf2061ad393af176c
Robert Oguzov <robertmama20@gmail.com>
2024-04-28 17:21:20 +0300

    Создание бина UserDetailsService

--- src/main/java/com/example/tacocloud/web/WebConfig.java
+++ src/main/java/com/example/tacocloud/web/WebConfig.java
@@ -1,9 +1,19 @@
 package com.example.tacocloud.web;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableAutoConfiguration
@@ -12,5 +22,19 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        List<UserDetails> usersList = new ArrayList<>();
+
+        usersList.add(new User(
+                "robert", encoder.encode("password"),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))));
+        usersList.add(new User(
+                "shama", encoder.encode("password"),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))));
+
+        return new InMemoryUserDetailsManager(usersList);
     }
 }
